92991b4ca027d2a9bc276901a639ac3b14be6886
Nam Tran Dev <namtran09061992@gmail.com>
2018-12-30 19:08:59 +0700

    - Optimize detail feature

--- app/src/main/java/dev/tran/nam/gobear/view/main/detail/DetailFragment.kt
+++ app/src/main/java/dev/tran/nam/gobear/view/main/detail/DetailFragment.kt
@@ -15,6 +15,8 @@ class DetailFragment : BaseFragmentInjection() {
 
     private lateinit var mViewDataBinding: FragmentDetailBinding
 
+    private var article : ArticleModel? = null
+
     public override fun layoutId(): Int {
         return R.layout.fragment_detail
     }
@@ -32,15 +34,18 @@ class DetailFragment : BaseFragmentInjection() {
             onBack()
         }
 
-        val article = arguments?.getSerializable(Constant.ARGUMENT_KEY_ARTICLE)
+        article = arguments?.getSerializable(Constant.ARGUMENT_KEY_ARTICLE) as ArticleModel
         article.run {
             mViewDataBinding.article = this as ArticleModel
-
-            mViewDataBinding.webview.loadUrl(this.link)
         }
     }
 
-    fun onBack(){
+    override fun initialized() {
+        super.initialized()
+        mViewDataBinding.webview.loadUrl(article?.link)
+    }
+
+    private fun onBack(){
         activity?.onBackPressed()
     }
 }
